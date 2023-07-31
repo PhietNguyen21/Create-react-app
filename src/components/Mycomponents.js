08829f4fd22276ba6ae3327cd3759e6016962aa6
Nphiet <phietnguyen321@gmail.com>
2023-07-31 16:35:53 +0700

    video 19

--- src/components/Mycomponents.js
+++ src/components/Mycomponents.js
@@ -5,12 +5,24 @@ class Mycomponent extends React.Component {
     name: "Phiet",
     age: 22,
   };
+
+  handelClick(event) {
+    // console.log("Click me");
+    // console.log(event.target);
+  }
+  handelOnmouseover(e) {
+    console.log(e.pageX);
+  }
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name}
         and I'm {this.state.age} years old
+        <br />
+        <button onMouseOver={this.handelOnmouseover} onClick={this.handelClick}>
+          Click me
+        </button>
       </div>
     );
   }
