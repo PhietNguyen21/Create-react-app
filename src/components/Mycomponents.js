a978b17724d3c113f965b025ddeaba56a842e898
Nphiet <phietnguyen321@gmail.com>
2023-07-31 15:44:58 +0700

    video 17

--- src/components/Mycomponents.js
+++ src/components/Mycomponents.js
@@ -1,9 +1,18 @@
 // class component
 import React from "react";
 class Mycomponent extends React.Component {
+  state = {
+    name: "Phiet",
+    age: 22,
+  };
   // JSX
   render() {
-    return <div>My first component</div>;
+    return (
+      <div>
+        My name is {this.state.name}
+        and I'm {this.state.age} years old
+      </div>
+    );
   }
 }
 
