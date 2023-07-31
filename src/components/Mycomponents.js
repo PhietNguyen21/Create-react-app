5c8a309dd5836a2a6ca4b7dc417c704b0543fb77
Nphiet <phietnguyen321@gmail.com>
2023-07-31 15:20:03 +0700

    update

---
+++ src/components/Mycomponents.js
@@ -0,0 +1,10 @@
+// class component
+import React from "react";
+class Mycomponent extends React.Component {
+  // JSX
+  render() {
+    return <div>My first component</div>;
+  }
+}
+
+export default Mycomponent;
