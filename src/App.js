d864da02ce670f20fc291e21306339290252d77d
Nphiet <phietnguyen321@gmail.com>
2023-08-07 20:14:31 +0700

    video 46 Setup Bootstrap & React Router Dom

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import Mycomponent from "./components/Mycomponents";
 import React from "react";
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello World &amp; Phiet Nguyen
-        <Mycomponent></Mycomponent>
-      </div>
-    );
-  }
-}
+
+const App = () => {
+  return (
+    <>
+      <div className="app-container">hello world</div>
+    </>
+  );
+};
 
 export default App;
