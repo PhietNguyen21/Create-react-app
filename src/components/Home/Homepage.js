f51dff1e5f6d37639672219986279e8e00e0cc62
Nphiet <phietnguyen321@gmail.com>
2023-08-10 10:38:16 +0700

    video 53 Design New Header

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -3,9 +3,23 @@ import videoHomePage from "../../assets/video-homepage.mp4";
 const Homepage = (props) => {
   return (
     <div className="homepage-container">
-      <video autoPlay loop>
-        <source src={videoHomePage} type="video/mp4" />
-      </video>
+      <div className="homepage">
+        <video autoPlay loop muted>
+          <source src={videoHomePage} type="video/mp4" />
+        </video>
+
+        <div className="homepage-content">
+          <h3 className="tilte">There's a better way to ask</h3>
+          <p className="title-content">
+            You don't want to make a boring form. And your audience won't answer
+            one.Create a typeform instead -- and make every happy.
+          </p>
+
+          <div>
+            <button className="btn-free">Get's started.It's free</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
