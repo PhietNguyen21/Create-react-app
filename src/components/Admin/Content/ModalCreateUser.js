d0dbe671570ccb8b02fe644338e9a2c80de8598c
Nphiet <phietnguyen321@gmail.com>
2023-08-14 09:55:57 +0700

    video 57 State Modal Add New User

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { FcPlus } from "react-icons/fc";
 
 const ModalCreateUser = (props) => {
   const [show, setShow] = useState(false);
@@ -8,6 +9,22 @@ const ModalCreateUser = (props) => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [userName, setUserName] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handelUploadImg = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(e.target.files[0]));
+      setImage(e.target.files[0]);
+    }
+    // else {
+    //   setPreviewImage(e.target.files[0]);
+    // }
+  };
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -22,27 +39,76 @@ const ModalCreateUser = (props) => {
           <form className="form-container">
             <div className="form-group">
               <label>Email</label>
-              <input type="email" name="mail" id="email" />
+              <input
+                type="email"
+                name="mail"
+                id="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </div>
             <div className="form-group">
               <label>Password</label>
-              <input type="password" name="pass" id="pass" />
+              <input
+                type="password"
+                name="pass"
+                id="pass"
+                value={pass}
+                onChange={(e) => {
+                  setPass(e.target.value);
+                }}
+              />
             </div>
             <div className="form-group">
               <label>User Name</label>
-              <input type="text" name="nameUser" id="nameUser" />
+              <input
+                type="text"
+                name="nameUser"
+                id="nameUser"
+                value={userName}
+                onChange={(e) => {
+                  setUserName(e.target.value);
+                }}
+              />
             </div>
             <div className="form-group role">
               <label>Role</label>
-              <select id="state">
-                <option value="vietnam">VietNam</option>
-                <option value="japan">JAPAN</option>
+              <select
+                id="state"
+                onChange={(e) => {
+                  setRole(e.target.value);
+                }}
+              >
+                <option value="USER">USER</option>
+                <option value="ADMIN">ADMIN</option>
               </select>
             </div>
 
             <div className="form-image">
-              <label>Image</label>
-              <input type="file" name="img" id="img" />
+              <label className="label-upload" htmlFor="labelUpload">
+                <div>
+                  <FcPlus />
+                  <span>Upload Image</span>
+                </div>
+              </label>
+              <input
+                type="file"
+                name="img"
+                id="labelUpload"
+                hidden
+                onChange={(e) => {
+                  handelUploadImg(e);
+                }}
+              />
+              <div className="img-preview">
+                {previewImage ? (
+                  <img src={`${previewImage}`} alt="test" />
+                ) : (
+                  <span>Preview Image</span>
+                )}
+              </div>
             </div>
           </form>
         </Modal.Body>
