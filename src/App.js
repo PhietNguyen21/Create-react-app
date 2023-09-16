92bd350881026c940a84361ca99a6cd23ad16897
Nphiet <phietnguyen321@gmail.com>
2023-09-16 10:58:40 +0700

    bugs

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Header from "./share/Header";
+import { Home } from "@mui/icons-material";
 const App = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/share/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Aave } from "iconsax-react-native";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <Aave size="32" color="#FF8A65" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
